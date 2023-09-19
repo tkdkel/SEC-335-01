9cbbdf00459410ce64a006ff84acd0b25f6e7629
tkdkel <kel.kruk@mymail.champlain.edu>
2023-09-19 13:39:04 -0400

    Week 3 DNS enumeration

--- Week3/dns-resolver.sh
+++ Week3/dns-resolver.sh
@@ -9,12 +9,12 @@ if [ -z "$network_prefix" ] || [ -z "$dns_server" ]; then
     exit 1
 fi
 
-# Loop through .2 to .254 and perform an nslookup
+# Perform nslookup with a loop through .2 - .254
 for i in $(seq 2 254); do
     host="$network_prefix.$i"
     result=$(nslookup $host $dns_server 2>&1)
     
-    # Only show results that don't contain "can't find"
+    # Ignore results that contain "can't find"
     if ! echo "$result" | grep -q "can't find"; then
         echo "$result"
     fi
